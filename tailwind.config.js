7c0bd811666728075b4e87c9d27ff291879c0991
Aza77 <azarielrosales77@gmail.com>
2025-06-16 14:40:30 -0400

    first version about us

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./public/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: { keyframes: {
+      slideInLeft: {
+        '0%': { transform: 'translateX(100%)', opacity: '0' },
+        '100%': { transform: 'translateX(0)', opacity: '1' },
+      },
+    },
+    animation: {
+      slideInLeft: 'slideInLeft 0.5s ease-out forwards',
+    },
+  },
   },
   plugins: [],
 };
